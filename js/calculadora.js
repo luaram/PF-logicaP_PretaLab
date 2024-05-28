dd94921004a85094e973afb373b2f3aa82c6c4b8
luaram <luara.mariano@hotmail.com>
2024-05-28 19:11:04 -0300

    Primeiro commit

--- js/calculadora.js
+++ js/calculadora.js
@@ -3,11 +3,13 @@ function somar() {
     const numero2 = parseFloat(document.getElementById("numero2Soma").value);
     // evite mexer no código acima!
   
-    //escreva seu código aqui
+    let resultado= numero1+numero2;
+   
+    
   
     /* use as constantes numero 1 e numero 2 e realize a soma entre elas.
     Em seguida, ao invés de mostrar o numero 10, mostre o resultado da operação */
-    document.getElementById("resultadoSoma").innerHTML = resultado;
+  console.log=document.getElementById("resultadoSoma").innerHTML = resultado;
 }
 
 function subtrair() {
@@ -15,7 +17,7 @@ function subtrair() {
     const numero2 = parseFloat(document.getElementById("numero2Sub").value);
     // evite mexer no código acima!
 
-    //escreva seu código aqui
+    let resultado= numero12-numero2;
 
     /* use as constantes numero 1 e numero 2 e realize a subtração entre elas.
         Em seguida, ao invés de mostrar o numero 9, mostre o resultado da operação */
@@ -27,7 +29,7 @@ function multiplicar() {
     const numero2 = parseFloat(document.getElementById("numero2Mult").value);
     // evite mexer no código acima!
 
-    //escreva seu código aqui
+    let resultado= numero1-numero2;
 
     /* use as constantes numero 1 e numero 2 e realize a multiplicação entre elas.
         Em seguida, ao invés de mostrar o numero 5, mostre o resultado da operação */
@@ -39,7 +41,7 @@ function dividir() {
     const numero2 = parseFloat(document.getElementById("numero2Div").value);
     // evite mexer no código acima!
 
-    //escreva seu código aqui
+    let resultado=numero1/numero2;
 
     /* use as constantes numero 1 e numero 2 e realize a divisão entre elas.
         Em seguida, ao invés de mostrar o numero -89, mostre o resultado da operação */
--- js/media.js
+++ js/media.js
@@ -18,8 +18,20 @@ function calcularMedia() {
     const nota4 = parseFloat(document.getElementById("nota4").value);
   
     let mensagem = "";
-    // evite mexer no código acima!
-  
+    const media=(nota1+nota2+nota3+nota4)/4;
+    if (media === 0) {
+      mensagem = 'Infelizmente você zerou a prova :( ';
+  } else if (media >= 0.1 && media <= 3) {
+      mensagem = `Caramba, deu ruim, você obteve média ${media}! Estude mais e tente novamente!`;
+  } else if (media >= 3.1 && media <= 5.9) {
+      mensagem = `Você obteve média ${media}! Falta pouco para a média.`;
+  } else if (media >= 6 && media <= 7) {
+      mensagem = `Você está na média com ${media}`;
+  } else if (media >= 7.1 && media <= 9.9) {
+      mensagem = `Notão! Sua média é ${media}!`;
+    } else if (media === 10) {
+      mensagem = '<p style="text-align: center;">Hoje é seu aniversário? Porque você está de parabéns! 10 de média! <i class="far fa-handshake"></i></p>';
+  }
   
   
   
